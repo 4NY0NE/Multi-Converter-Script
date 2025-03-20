ed57161e11a9d75e0cb6dd9ec7a5d1d9ba2a4a14
B13L <104938446+4NYONEx@users.noreply.github.com>
2025-03-20 00:12:53 -0300

    Add files via upload

---
+++ ExE-Converter.ps1
@@ -0,0 +1,30 @@
+# ======================================================#
+#         ___  _   _ __   __ _____  _   _  _____ 
+#        /   || \ | |\ \ / /|  _  || \ | ||  ___|
+#       / /| ||  \| | \ V / | | | ||  \| || |__
+#      / /_| || . ` |  \ /  | | | || . ` ||  __|
+#      \___  || |\  |  | |  \ \_/ /| |\  || |___
+#          |_/\_| \_/  \_/   \___/ \_| \_/\____/
+#     Follow me on github: https://github.com/4NYONEx                                    
+# ======================================================#
+
+$ps1Path = Read-Host "Enter the full path of the PS1 file"
+$exePath = Read-Host "Enter the full path where the EXE will be saved"
+
+if (-Not (Test-Path $ps1Path)) {
+    Write-Host "Error: The PS1 file was not found!"
+    exit
+}
+
+Write-Host "Converting $ps1Path to $exePath..."
+Start-Process -NoNewWindow -Wait -FilePath "powershell" -ArgumentList "-Command ps2exe `"$ps1Path`" `"$exePath`""
+
+if (Test-Path $exePath) {
+    Write-Host "Conversion completed successfully! The EXE file was created at: $exePath"
+}
+else {
+    Write-Host "Error creating the EXE."
+}
+
+Write-Host "Press any key to exit..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
